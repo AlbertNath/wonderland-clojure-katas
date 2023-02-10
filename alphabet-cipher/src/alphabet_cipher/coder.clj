41b829758d339484a9d0a2df9261f0cc4e330fa5
Alberto Natanael Medel Piña <natanael_pi@ciencias.unam.mx>
2023-02-09 22:20:07 -0600

    Start alphabet cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,17 @@
 (ns alphabet-cipher.coder)
 
+;; Función para
+(defn char-to-int [ch] (- (int ch) 97))
+
+;; función inversa de la anterior
+(defn int-to-char [idx] (char (+ idx 97)))
+
+
 (defn encode [keyword message]
-  "encodeme")
+  (str map-indexed #() message))
 
 (defn decode [keyword message]
   "decodeme")
 
 (defn decipher [cipher message]
   "decypherme")
-
