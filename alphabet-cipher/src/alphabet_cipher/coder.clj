facfffacbf810bad9713f395d33b1490148cfdd2
Alberto Natanael Medel Piña <natanael_pi@ciencias.unam.mx>
2023-02-11 16:44:37 -0600

    Some progress

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,14 +1,25 @@
 (ns alphabet-cipher.coder)
 
 ;; Función para
-(defn char-to-int [ch] (- (int ch) 97))
+(defn char-to-int
+  "Función para convertir un carácter en
+  representación decimal ASCII. Restamos 97
+  pues el alfbeto minúsuclo empieza en este
+  índice."
+  [ch]  (- (int ch) 97))
 
 ;; función inversa de la anterior
-(defn int-to-char [idx] (char (+ idx 97)))
+(defn int-to-char
+  "Función inversa a la anterior"
+  [idx] (char (+ idx 97)))
 
 
 (defn encode [keyword message]
   (str map-indexed #() message))
+
+(defn test-map [word, key]
+  (str map-indexed #((int-to-char ((char-to-int (nth word %1)) +
+                                   (char-to-int (nth key (mod %1 (count key))))))) word))
 
 (defn decode [keyword message]
   "decodeme")
