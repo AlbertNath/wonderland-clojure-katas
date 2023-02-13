fb0c2e5165ff95dedfb7a764bac47242b7b23d0d
Alberto Natanael Medel Piña <natanael_pi@ciencias.unam.mx>
2023-02-13 17:28:32 -0600

    Alphabet-cipher done.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -10,6 +10,24 @@
   "Función inversa a la anterior"
   [idx] (char (+ idx 97)))
 
+(defn partWord
+  "Función  que, dada  una cadena  de  entrada `s`  y  un entero  `n`, toma  una
+  subcadena de longitud `n` y la repite por la longitud total de la cadena `s`"
+  [s n]
+  (let [length (count s)]
+    (apply str (take length (apply str (repeat length (subs s 0 n)))))))
+
+(defn exctractStr
+  "Usando la función auxiliar `partWord`,  itera tantas veces sea necesario para
+  detereminar la longitud de la cadena original con la que se forma la cadena de
+  entrada `s`."
+  [s]
+  (loop [n 1]
+    (let [w (partWord s n)]
+     (if (= s w)
+      (subs s 0 n)
+      (recur (inc n))))))
+
 (defn encode
   "La idea  es iterar por la  cadena a cifrar  y usar los carácteres  ASCII como
   índices; el  cruce entre  el valor del  carácter en `idx`  del `mensaje`  y el
@@ -19,7 +37,8 @@
   [keyword message]
   (apply str (map-indexed (fn [idx _]
                             (int-to-char (mod (+ (char-to-int (nth message idx))
-                                                 (char-to-int (nth keyword (mod idx (count keyword))))) 26))) message)))
+                                                 (char-to-int (nth keyword
+                                                                   (mod idx (count keyword))))) 26))) message)))
 (defn decode
   "Al ser la  función inversa de la  anterior basta, en vez de  sumar, restar al
   valor  del carácter  en `idx`  de `keyword`  al valor  en el  mismo índice  en
@@ -27,7 +46,8 @@
   [keyword message]
   (apply str (map-indexed (fn [idx _]
                             (int-to-char (mod (- (char-to-int (nth message idx))
-                                                 (char-to-int (nth keyword (mod idx (count keyword))))) 26))) message)))
+                                                 (char-to-int (nth keyword
+                                                                   (mod idx (count keyword))))) 26))) message)))
 
 (defn decipher
   "La fórmula  que nos  da el  carácter de la  `keyword` en  el índice  `idx` es
@@ -35,7 +55,6 @@
   la función  inversa de `mod` es  $a + x =  0 mod m$ (aunque  probablemente hay
   formas más elegantes)."
   [cipher message]
-  (apply str (distinct (apply str (map-indexed (fn [idx _]
+  (exctractStr (apply str (map-indexed (fn [idx _]
                                  (int-to-char (mod (+ (- 26 (char-to-int (nth message idx)))
-                                                      (char-to-int (nth cipher idx))) 26))) message)))))
-(decipher "opkyfipmfmwcvqoklyhxywgeecpvhelzg" "thequickbrownfoxjumpsoveralazydog")
+                                                      (char-to-int (nth cipher idx))) 26))) message))))
